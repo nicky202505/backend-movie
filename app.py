c906f803b172d27a35c25429ef89f64e699be559
nicky202505 <jangnicole878@gmail.com>
2025-09-26 10:39:33 +0900

    gg

--- app.py
+++ app.py
@@ -12,7 +12,9 @@ import uuid
 # Flask 앱 생성
 app = Flask(__name__)
 # CORS 설정 - 프론트엔드에서 오는 요청 허용
-CORS(app, resources={r"/chat": {"origins": ["http://localhost:5000", "http://localhost:3000", "http://localhost:5173","https://movie-fr-six.vercel.app"]}})
+CORS(app, resources={r"/chat": {"origins": ["http://localhost:5000", "http://localhost:3000", "http://localhost:5173", "https://vercel.com/nicoles-projects-4579d544/frontend-movie/DyeUNqBajqDUXKxVY9GehZJhELtp", "https://frontend-movie-eta.vercel.app/"]}})
+
+
 
 # 환경변수에서 키 읽어오기
 CLOVA_INVOKE_URL = os.environ.get('CHATBOT_INVOKE_URL')
